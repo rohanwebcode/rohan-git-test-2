4df0bee9f1c856c0b63a6f28f874a8d3b60066fe
Rohan Isagani <rohanisagani4@gmail.com>
2025-02-14 19:44:14 +0700

    menambahkan daftar list istilah-istilah yang yang ada di github.

--- index.php
+++ index.php
@@ -8,5 +8,14 @@
 <body>
     <h1>Heloo world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit fugiat adipisci totam, mollitia eveniet, quos excepturi natus quis perspiciatis voluptatibus repudiandae culpa, odio repellat dolorem consequatur optio impedit sed libero?</p>
+    <hr>
+    <h3>Berikut adalah daftar istilah-istilah yang umum digunkan dalam proses upload data local ke repository github</h3>
+    <ul>
+        <li>⚠️ Merge Conflicts: Mengatasi konflik saat ada perubahan bersamaan.</li>
+        <li>🔄 Push: Mengirimkan perubahan dari lokal ke remote.</li>
+        <li>📥 Pull & Fetch: Mengambil perubahan dari remote ke lokal.</li>
+        <li>💻 Cloning: Mendapatkan salinan repositori dari GitHub ke lokal.</li>
+        <li>🌐 Git Remote: Menghubungkan repositori lokal dengan GitHub.</li>
+    </ul>
 </body>
 </html>
