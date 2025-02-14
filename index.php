f2cca79b74806980dfa1194f51058ec404706eb2
Rohan Isagani <rohanisagani4@gmail.com>
2025-02-14 17:42:39 +0700

    Menambahakan baris paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Heloo world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit fugiat adipisci totam, mollitia eveniet, quos excepturi natus quis perspiciatis voluptatibus repudiandae culpa, odio repellat dolorem consequatur optio impedit sed libero?</p>
 </body>
 </html>
